2b10d94ed8a3793ae39963c4b241a08180b4e73e
zhaoyi <zhaoyi@jsdeiMac.local>
2019-01-08 21:07:40 +0800

    ,

--- src/app/counter/counter.page.ts
+++ src/app/counter/counter.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-counter',
@@ -7,9 +9,126 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterPage implements OnInit {
 
-  constructor() { }
+  list = [];
+  className = '';
+  originValue = '';
+  preValue = '';
+  currentValue = '';
+  result: number;
+  operation = '';
+  isCounted = false;
+
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.createList();
+  }
+
+  createList() {
+    this.list = [
+      { items: ['C'], opt: [], isBack: true },
+      { items: ['1', '2', '3'], opts: ['×'] },
+      { items: ['4', '5', '6'], opts: ['÷'] },
+      { items: ['7', '8', '9'], opts: ['-'] },
+      { items: ['0', '.'], opts: ['+', '＝'] },
+    ];
+  }
+
+  goBack() {
+    this.navCtrl.goBack();
+  }
+
+  clear() {
+    this.preValue = '';
+    this.currentValue = '';
+    this.result = null;
+    this.operation = '';
+  }
+
+  onStart(item: any, e: any, data: any) {
+    if (item === 'C') {
+      this.clear();
+      return;
+    }
+    const isOpts = this.isOperation(item, data);
+    if (this.isCounted) {
+      this.reset(isOpts);
+    }
+    if (item === '＝') {
+      this.doCount();
+      return;
+    }
+    if (!this.operation) {
+      if (!isOpts) {
+        if (this.canAdd(this.preValue, item)) {
+          this.preValue += item;
+        }
+      } else {
+        if (item && item !== '＝' && item !== 'C') { this.operation = item; }
+      }
+    } else {
+      if (!isOpts) {
+        if (this.canAdd(this.preValue, item)) {
+          this.currentValue += item;
+        }
+      } else {
+        if (item && item !== '＝' && item !== 'C') { this.operation = item; }
+      }
+    }
+    if (data.isBack) {
+      if (this.operation) {
+        this.currentValue = this.currentValue.substr(0, this.currentValue.length - 1);
+      } else {
+        this.preValue = this.preValue.substr(0, this.preValue.length - 1);
+      }
+    }
+    this.className = e.target.className;
+    if (!data.isBack) {
+      e.target.className += ' active';
+    }
+  }
+
+  canAdd(val: string, item: string): boolean {
+    return (val.length || item !== '.') && val[0] !== '.' && val.indexOf('.') < 0;
+  }
+
+  reset(isOpts: boolean) {
+    if (isOpts) {
+      this.preValue = this.result.toString();
+      this.result = null;
+      this.operation = '';
+      this.currentValue = '';
+    } else {
+      this.clear();
+    }
+    this.isCounted = false;
+  }
+
+  isOperation(val: string, data: any): boolean {
+    const opts = ['C', '×', '÷', '-', '+', '＝'];
+    return data.isBack || opts.includes(val);
+  }
+
+  doCount() {
+    switch (this.operation) {
+      case '×':
+        this.result = Number(this.preValue) * Number(this.currentValue);
+        break;
+      case '÷':
+        this.result = Number(this.preValue) / Number(this.currentValue);
+        break;
+      case '-':
+        this.result = Number(this.preValue) - Number(this.currentValue);
+        break;
+      case '+':
+        this.result = Number(this.preValue) + Number(this.currentValue);
+        break;
+    }
+    this.isCounted = true;
+  }
+
+  onEnd(e) {
+    e.target.className = this.className;
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { ToastController, NavController } from '@ionic/angular';
 
@@ -7,16 +7,16 @@ import { ToastController, NavController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  segment: number = 1;
+  segment = 1;
   list = [
-    { text: '按利率计算', value: 1 }, 
+    { text: '按利率计算', value: 1 },
     { text: '按每十万扣费计算', value: 2 }
   ];
   formlist1 = [
     { label: '票面金额', value: null, unit: '万元', holder: '多有多赚，少有少有盈' },
-    { label: '年利率', value: null,isYear: true, unit: '%', holder: '不多不少，合适就好' },
+    { label: '年利率', value: null, isYear: true, unit: '%', holder: '不多不少，合适就好' },
     { label: '月利率', value: null, unit: '‰', holder: '不多不少，合适就好' },
     { label: '手续费', value: null, unit: '元/十万', holder: '露一手，留一手' },
     { label: '打款费', value: null, unit: '元', holder: '请输入打款费' }
@@ -34,22 +34,22 @@ export class HomePage {
   dateEnd: string;
   isEndRed = false;
   isStartRed = false;
-  addDay:number = 0;
+  addDay = 0;
   cards: any[] = [];
   cards1 = [
-    [{title:'计息天数', canAdd: false, value:'--', unit:'天'},
-    {title:'每十万贴息', canAdd: true, value:'--', unit:'元'}],
-    [{title:'贴现利息', canAdd: true, value:'--', unit:'元'},
-    {title:'贴现金额',canAdd: true, value:'--', unit:'元'}]
-  ]
-
-  cards2 = [
-    [{title:'计息天数', canAdd: false, value:'--', unit:'天'},
-    {title:'折合年利率', canAdd: true, value:'--', unit:'%'}],
-    [{title:'折合月利率', canAdd: true, value:'--', unit:'‰'}]
+    [{ title: '计息天数', canAdd: false, value: '--', unit: '天' },
+    { title: '每十万贴息', canAdd: true, value: '--', unit: '元' }],
+    [{ title: '贴现利息', canAdd: true, value: '--', unit: '元' },
+    { title: '贴现金额', canAdd: true, value: '--', unit: '元' }]
   ];
 
-  constructor(  private toast: ToastController, private navCtrl: NavController) {
+  cards2 = [
+    [{ title: '计息天数', canAdd: false, value: '--', unit: '天' },
+    { title: '折合年利率', canAdd: true, value: '--', unit: '%' }],
+    [{ title: '折合月利率', canAdd: true, value: '--', unit: '‰' }]
+  ];
+
+  constructor(private toast: ToastController, private navCtrl: NavController) {
   }
 
   ngOnInit() {
@@ -63,10 +63,11 @@ export class HomePage {
   }
 
   setDays() {
-    const start = new Date(this.dateStart); 
+    const start = new Date(this.dateStart);
     const now = new Date(this.dateEnd);
-    const days = now.getTime() - start.getTime(); 
-    const day = parseInt(String(days / (1000 * 60 * 60 * 24))) + this.addDay; 
+    const days = now.getTime() - start.getTime();
+    // tslint:disable-next-line:radix
+    const day = parseInt(String(days / (1000 * 60 * 60 * 24))) + this.addDay;
     this.cards[0][0].value = day;
     this.calculate();
   }
@@ -75,21 +76,21 @@ export class HomePage {
     const si = new Date(this.dateStart).getDay();
     this.startDay = this.dayNames[si];
     this.isStartRed = si === 0 || si === 6 || false;
-    if (!this.dateEnd) return;
+    if (!this.dateEnd) { return; }
     const ei = new Date(this.dateEnd).getDay();
     this.endDay = this.dayNames[ei];
     this.isEndRed = ei === 0 || ei === 6 || false;
   }
- 
+
   onPick() {
-    if(new Date(this.dateStart).getTime() >new Date(this.dateEnd).getTime()) {
-      const toast = this.toast.create({
+    if (new Date(this.dateStart).getTime() > new Date(this.dateEnd).getTime()) {
+      this.toast.create({
         message: '贴现日期不能大于到期日期',
         duration: 3000,
-        mode:'ios',
-        cssClass:'cus-toast',
+        mode: 'ios',
+        cssClass: 'cus-toast',
         position: 'middle',
-      }).then( t => {
+      }).then(t => {
         t.present();
       });
       return;
@@ -101,7 +102,7 @@ export class HomePage {
   }
 
   onIncres() {
-    if(this.addDay == 0) return;
+    if (this.addDay === 0) { return; }
     this.addDay -= 1;
     this.setDays();
   }
@@ -113,9 +114,9 @@ export class HomePage {
 
   // 按利率计算
   calculate(item?: any) {
-    if(this.segment == 2) {
+    if (this.segment === 2) {
       // 折合年利率 = 十万扣费*360 / 计息天数 /100000*100
-      if(this.formlist2[0].value == '' || this.formlist2[0].value == null) return;
+      if (this.formlist2[0].value === '' || this.formlist2[0].value == null) { return; }
       const a = this.formlist2[0].value * 360 / this.cards[0][0].value;
       const b = a / 100000 * 100;
       this.cards[0][1].value = b.toFixed(8);
@@ -124,34 +125,34 @@ export class HomePage {
       this.cards[1][0].value = (b / 1.2).toFixed(8);
 
     } else {
-      if(item && item.isYear) {
-        this.formlist1[2].value = (item.value/12).toFixed(8);
+      if (item && item.isYear && item.value) {
+        this.formlist1[2].value = (item.value / 12).toFixed(8);
       }
-      if(this.formlist1[0].value == '' || this.formlist1[0].value == null) return;
-      if(this.formlist1[1].value == '' || this.formlist1[1].value == null) return;
-  
+      if (this.formlist1[0].value === '' || this.formlist1[0].value == null) { return; }
+      if (this.formlist1[1].value === '' || this.formlist1[1].value == null) { return; }
+
       // 贴现利息 =（100000*年利率/360/100*计息天数+手续费）*票面金额/10
       const c = this.formlist1[1].value;
-      const a = (100000 * c)/360/100;
+      const a = (100000 * c) / 360 / 100;
       const b = a * this.cards[0][0].value + this.formlist1[3].value;
-      const d = b * this.formlist1[0].value/10;
+      const d = b * this.formlist1[0].value / 10;
       this.cards[1][0].value = d.toFixed(2);
-  
+
       // 贴现金额= 票面金额*10000 - 贴现利息 - 打款费
-      const m =this.formlist1[4].value || 0;
-      const e = this.formlist1[0].value*10000 - this.cards[1][0].value;
+      const m = this.formlist1[4].value || 0;
+      const e = this.formlist1[0].value * 10000 - this.cards[1][0].value;
       const f = e - m;
       this.cards[1][1].value = f;
-  
+
       // 每十万贴息
-      this.cards[0][1].value = ( d * 10 ).toFixed(2);
+      this.cards[0][1].value = (d * 10).toFixed(2);
     }
-   
+
   }
 
   // 按每十万扣费计算
   segmentChanged(item) {
-    if(this.segment == item.value) return;
+    if (this.segment === item.value) { return; }
     switch (item.value) {
       case 2:
         this.cards2[0][0].value = this.cards1[0][0].value;
@@ -169,7 +170,7 @@ export class HomePage {
   onSkip(item) {
     this.navCtrl.navigateForward('/counter', {
       queryParams: {
-        value:item.value
+        value: item.value
       }
     });
   }
